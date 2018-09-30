1facb2aa2d9dcc7b8d47f820bbcc8a5e9cb7c87b
moralok <moralok@163.com>
2018-09-30 18:03:05 +0800

    User can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -23,4 +23,9 @@ class ReplyObserver
     {
         $reply->content = clean($reply->content, 'user_topic_body');
     }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1);
+    }
 }
