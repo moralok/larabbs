0d80a216ea4f06e68ac3d98ec11c326ad0ae9957
moralok <moralok@163.com>
2018-10-09 14:40:22 +0800

    Admin permissions

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
